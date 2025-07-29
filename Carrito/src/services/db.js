e0e22c76e25aee66322858c1c857324851693398
Alfa06N <nicolasalfag@gmail.com>
2025-07-28 22:13:30 -0400

    Add db.js to de backend services

--- Carrito/src/services/db.js
+++ Carrito/src/services/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "password",
+  password: "password", // Cambia esto por la tuya
   database: "shopping_cart",
   waitForConnections: true,
   connectionLimit: 10,
